a1f74dadde853f9b651ae693ffcdcab25ee1aafb
rajat128 <155458603+rajat128@users.noreply.github.com>
2024-07-24 19:58:24 +0530

    Update connect.js

--- DB/connect.js
+++ DB/connect.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const connectDB=async()=>{
    
-    console.log(process.env.MONGOOSE_URI);
+    
         const connectionInstance=await mongoose.connect("mongodb+srv://Rajat:Rajat1234@cluster0.l2yyhxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/expenses")
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
   
